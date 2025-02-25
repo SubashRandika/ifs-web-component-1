63b71b7d55add36f9fc7e66ed4be4b7dffc6b1d8
Randika Rodrigo <randika.rodrigo@ifs.com>
2025-02-25 14:51:58 +0530

    update base path in Vite configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/your-repo-name/", // Adjust to your GitHub repository name
+    base: "/ifs-web-component-1/", // Adjust to your GitHub repository name
     build: {
         outDir: "docs", // Output the build to `docs` folder for GitHub Pages
     },
