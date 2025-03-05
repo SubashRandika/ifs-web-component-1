d2b4a9c6e98af1038fc661af580abe9b911f28b7
Randika Rodrigo <randika.rodrigo@ifs.com>
2025-03-05 20:25:44 +0530

    chore: bump version to 1.0.3 in package.json and comment out base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    base: "/ifs-web-component-1/", // Adjust to your GitHub repository name
+    // base: "/ifs-web-component-1/", // Adjust to your GitHub repository name
     build: {
         // outDir: "docs", // Output the build to `docs` folder for GitHub Pages
         lib: {
